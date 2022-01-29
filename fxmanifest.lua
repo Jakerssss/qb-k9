a58e0bc6e171c581b6e53c0d17743f6d3c6ebc22
Jakerssss <98612290+Jakerssss@users.noreply.github.com>
2022-01-29 12:36:35 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'K-9 Script for QB original author hashx_k9 edited by FjamZoo#0001'
+description 'K-9 Script for QB original author hashx_k9 edited by FjamZoo#0001, edited by JAKERS#6552'
 version '1.0.0'
 
 shared_script {
-	'@qb-core/shared/locale.lua',
-	'locales/en.lua',
 	'config.lua',
 }
 
